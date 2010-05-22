a0da0a387daac291813ef8e70828ab746b424bea
Rotem Yaari <vmalloc@gmail.com>
2010-05-22 17:39:33 +0300

    boilerplate

---
+++ run_tests.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+import os
+import sys
+import subprocess
+
+base_dir= os.path.realpath(os.path.join(os.path.dirname(__file__)))
+test_dir = os.path.join(base_dir, base_dir)
+sys.path.insert(0, base_dir)
+
+if __name__ == '__main__':
+    p = subprocess.Popen(["nosetests", test_dir], shell=True, env=dict(PYTHONPATH=":".join(sys.path)))
+    sys.exit(p.wait())
---
+++ setup.py
@@ -0,0 +1,8 @@
+## See LICENSE for license info
+from distutils.core import setup
+setup(name="growlpy",
+      description="Python bindings for the growl notification system",
+      author="Rotem Yaari",
+      author_email="vmalloc@gmail.com",
+      version="0.1",
+      packages=["growl"])
